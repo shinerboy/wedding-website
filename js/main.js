9dbcd7dfd765a03bcafe65ad17b0389b13d6b61a
shinerboy <34726251+shinerboy@users.noreply.github.com>
2026-03-26 20:05:38 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -230,3 +230,15 @@
 
 
 }());
+
+$(document).ready(function() {
+    if ($('.popup-youtube').length > 0) {
+        $('.popup-youtube').magnificPopup({
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false
+        });
+    }
+});
